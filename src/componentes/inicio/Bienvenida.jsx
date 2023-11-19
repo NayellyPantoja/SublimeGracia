44411252de7af64c3991d23c30efd1dded3b7206
Nayelly <nayellypantoja2000@gmail.com>
2023-11-18 21:18:55 -0500

    se agregan estilos para el componente bienvenida

--- src/componentes/inicio/Bienvenida.jsx
+++ src/componentes/inicio/Bienvenida.jsx
@@ -2,11 +2,11 @@ import imgBack from "../../assets/imgWebp/Backgrounds/BackgroundHome.webp";
 
 const Bienvenida = () => {
   return (
-    <>
+    <div className="containerBienvenida">
       <div className="containerImgBgHome">
         <img className="backgroundHome" src={imgBack} alt="" />
       </div>
-      <div className="containerBienvenida">
+      <div className="containerTextoBienvenida">
         <h1 className="tituloBienvenida">
           ¡Bienvenidos a <span>Sublime Gracia!</span>
         </h1>
@@ -14,7 +14,7 @@ const Bienvenida = () => {
           Más que una iglesia, somos una familia.
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
