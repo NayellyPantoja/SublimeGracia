4bc3dae9a296543247fcc6f520fb0ae7e86e46d9
Nayelly <nayellypantoja2000@gmail.com>
2023-11-23 15:41:26 -0500

    se realiza maquetado de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,146 +1,78 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "../styles/App.css";
 import {
-    Box,
-    Button,
-    FormControl,
-    Grid,
-    IconButton,
-    InputAdornment,
-    InputLabel,
-    OutlinedInput,
-    TextField,
-    Tooltip,
-    Typography,
-  } from "@mui/material";
-  import GoogleIcon from "@mui/icons-material/Google";
-  
-  import { Link, useNavigate } from "react-router-dom";
-  import { Visibility, VisibilityOff } from "@mui/icons-material";
-  import { useState } from "react";
-  
-  const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
-    const navigate = useNavigate();
-  
-    const handleClickShowPassword = () => setShowPassword(!showPassword);
-  
-    return (
-      <Box
-        sx={{
-          width: "100%",
-          minHeight: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <form>
-          <Grid
-            container
-            rowSpacing={2}
-            justifyContent={"center"}
-          >
-            <Grid item xs={10} md={12}>
-              <TextField name="email" label="Email" fullWidth />
-            </Grid>
-            <Grid item xs={10} md={12}>
-              <FormControl variant="outlined" fullWidth>
-                <InputLabel htmlFor="outlined-adornment-password">
-                  Contraseña
-                </InputLabel>
-                <OutlinedInput
-                  name="password"
-                  id="outlined-adornment-password"
-                  type={showPassword ? "text" : "password"}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        edge="end"
-                      >
-                        {showPassword ? (
-                          <VisibilityOff color="primary" />
-                        ) : (
-                          <Visibility color="primary" />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Contraseña"
-                />
-              </FormControl>
-            </Grid>
-            <Link
-              to="/forgot-password"
-              style={{ color: "steelblue", marginTop: "10px" }}
-            >
-              ¿Olvidaste tu contraseña?
-            </Link>
-            <Grid container justifyContent="center" spacing={3} mt={2}>
-              <Grid item xs={10} md={5}>
-                <Button
-                  variant="contained"
-                  fullWidth
-                  type="submit"
-                  sx={{
-                    color: "white",
-                    textTransform: "none",
-                    textShadow: "2px 2px 2px grey",
-                  }}
-                >
-                  Ingresar
-                </Button>
-              </Grid>
-              <Grid item xs={10} md={5}>
-                <Tooltip title="ingresa con google">
-                  <Button
-                    variant="contained"
-                    startIcon={<GoogleIcon />}
-                    type="button"
-                    fullWidth
-                    sx={{
-                      color: "white",
-                      textTransform: "none",
-                      textShadow: "2px 2px 2px grey",
-                    }}
-                  >
-                    Ingresa con google
-                  </Button>
-                </Tooltip>
-              </Grid>
-              <Grid item xs={10} md={8}>
-                <Typography
-                  color={"secondary.primary"}
-                  variant={"h6"}
-                  mt={1}
-                  align="center"
-                >
-                  ¿Aun no tienes cuenta?
-                </Typography>
-              </Grid>
-              <Grid item xs={10} md={5}>
-                <Tooltip title="ingresa con google">
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    onClick={()=>navigate("/register")}
-                    type="button"
-                    sx={{
-                      color: "white",
-                      textTransform: "none",
-                      textShadow: "2px 2px 2px grey",
-                    }}
-                  >
-                    Registrate
-                  </Button>
-                </Tooltip>
-              </Grid>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    );
-  };
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import imgLogin from "../assets/Imagenes/Login/loginbg.png";
 
-export default Login
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+
+  return (
+    <div className="containerForm">
+      <form className="cardContainer">
+        <img className="loginImage " src={imgLogin} />
+        <div className="formulario">
+          <div className="textLogin">
+            <h1>BIENVENIDO DE NUEVO</h1>
+          </div>
+
+          <Grid item xs={10} md={12} paddingBottom={"2rem"}>
+            <TextField name="email" label="Email" fullWidth />
+          </Grid>
+          <Grid item xs={10} md={12} paddingBottom={"2rem"}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Contraseña
+              </InputLabel>
+              <OutlinedInput
+                name="password"
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? (
+                        <VisibilityOff color="primary" />
+                      ) : (
+                        <Visibility color="primary" />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Contraseña"
+              />
+            </FormControl>
+          </Grid>
+          <div className="forgetPassword">
+          <Link>¿Olvidaste tu contraseña?</Link>
+          </div>
+          
+          
+          <div className="containerBoton">
+          
+            <button className="loginBoton">INICIAR SESIÓN</button>
+            <Link className="registro">Crear cuenta nueva</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
